4b3a919a0002f2211f379ac3c71976d2e9bc96b6
Benjamin Shapiro <benj@berkeley.edu>
2013-01-26 14:48:38 -0800

    modified for new database url

--- webapp/server.py
+++ webapp/server.py
@@ -2,13 +2,21 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://microfinance:hq7Np2Ex@/microfinance'
+from sqlalchemy import create_engine
+engine = create_engine('mysql://allie:967enm/allie', convert_unicode=True)
+
 db = SQLAlchemy(app)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    connection = engine.connect()
+    data = connection.execute('select * from sites')
+    connection.close()
+    string = ""
+    for row in data:
+        string += row + "</br>"
+    return string
 
 if __name__ == "__main__":
     app.run()
