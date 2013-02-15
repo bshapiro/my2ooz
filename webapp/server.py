fbfbbd9a1179151e3ac5c73722d2125eb5eba9e9
Benjamin Shapiro <benj@berkeley.edu>
2013-02-15 01:18:50 -0800

    mucho refactoring

--- webapp/server.py
+++ webapp/server.py
@@ -18,38 +18,45 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def test_all_calls():
+    
     connection = engine.connect()
 
+    # TESTS FOR WEEK INFO
     all_info = get_all_info(connection, 0)
     week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
     week_genres = []
     for day in week:
         week_genres.append(get_day_info(connection, day, 0))
 
-    connection.close()
-
     all_info_string = "all info for venue 0: " + all_info
     week_genres_string = ""
     for day in week:
         week_genres_string += day + ": " + week_genres[week.index(day)]
     final_string = all_info_string + week_genres_string
+    # END TESTS
+    
+    # TESTS FOR VENUE INFO
+    # END TESTS
 
+    connection.close()
     return final_string
 
 
 def get_all_info(connection, venue_id):
-    data = connection.execute('select * from venue_table where venue_id = ' + str(venue_id))
-    string = ""
-    for row in data:
-        string += str(row) + "</br>"
-    return string
+    query = 'select * from venue_table where venue_id = ' + str(venue_id)
+    data = connection.execute(query)
+    return stringerize(data)
 
 
 def get_day_info(connection, day, venue_id):
     query = 'select ' + day + ' from venue_table where venue_id = ' + str(venue_id)
     data = connection.execute(query)
+    return stringerize(data)
+
+
+def stringerize(sql_object):
     string = ""
-    for row in data:
+    for row in sql_object:
 	string += str(row) + "</br>"
     return string
 
