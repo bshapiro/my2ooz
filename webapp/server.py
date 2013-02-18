17bc69b0a7fcdeda49e7c6372be73a0e87cc6207
Benjamin Shapiro <benj@berkeley.edu>
2013-02-17 18:24:59 -0800

    created user class

--- webapp/server.py
+++ webapp/server.py
@@ -5,13 +5,20 @@ from subprocess import check_output
 import flask
 from flask import request
 from os import environ
+from flask.ext.login import (LoginManager, current_user, login_required,
+                            login_user, logout_user, UserMixin, AnonymousUser,
+                            confirm_login, fresh_login_required)
 
 from flask.ext.sqlalchemy import SQLAlchemy
 
 from sqlalchemy import create_engine
 
+
 app = flask.Flask(__name__)
 app.debug = True
+
+login_manager = LoginManager()
+
 engine = create_engine('mysql://my2ooz:w3rnid@/my200z', convert_unicode=True)
 db = SQLAlchemy(app)
 
@@ -20,20 +27,21 @@ db = SQLAlchemy(app)
 def test_all_calls():
     connection = engine.connect()
     # TESTS FOR WEEK INFO
-    all_info = get_all_info(connection, 0)
+    all_info = stringify(get_all_info(connection))
     week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
     week_genres = []
     for day in week:
-        week_genres.append(get_day_info(connection, day, 0))
+        week_genres.append(stringify(get_day_info_by_venue_id(connection, day, 0)))
 
-    all_info_string = "all info for venue 0: " + all_info
+    all_info_string = "all info:  " + all_info
     week_genres_string = ""
     for day in week:
         week_genres_string += day + ": " + week_genres[week.index(day)]
         
-    venue_info = get_venue_info(connection, 0)
+    venue_info = 'venue info for existing login: ' + stringify(get_venue_info_by_login(connection, 'login'))
+    venue_info_2 = 'venue info for non-existent login: ' + str(get_venue_info_by_login(conncetion, 'admin'))
     
-    final_string = all_info_string + week_genres_string + venue_info 
+    final_string = all_info_string + week_genres_string + venue_info + venue_info_2
     
     
     # END TESTS
@@ -44,27 +52,56 @@ def test_all_calls():
     connection.close()
     return final_string
 
-def get_all_info(connection, venue_id):
-    query = 'select * from venue_table where venue_id = ' + str(venue_id)
+def get_all_info(connection):
+    query = 'select * from venue_table'
     data = connection.execute(query)
-    return stringerize(data)
+    return data
 
 
-def get_day_info(connection, day, venue_id):
+def get_day_info_by_venue_id(connection, day, venue_id):
     query = 'select ' + day + ' from venue_table where venue_id = ' + str(venue_id)
     data = connection.execute(query)
-    return stringerize(data)
+    return data
 
-def get_venue_info(connection, venue_id):
-    query = 'select manager_name, food_drink from venue_table where venue_id = ' + str(venue_id)
-    data = connection.execute(query)
-    return stringerize(data)
+def get_venue_info_by_login(connection, login):
+    query = 'select * from venue_table where login = ' + str(login)
+    try:
+	data = connection.execute(query)
+    except Exception:
+	return None
+    return data
     
-def stringerize(sql_object):
+def row2dict(row):
+    return {c.name: getattr(row, c.name) for c in row.__table__.columns}
+
+def stringify(sql_object):
     string = ""
     for row in sql_object:
+	print type(row)
 	string += str(row) + "</br>"
     return string
+
+class User(UserMixin):
+
+    def __init__(self, venue_info, active=True)
+	self.name = venue_info['manager_name']
+	self.venue_id = venue_info['venue_id']
+	self.location_name = venue_info['location_name']
+	self.venue_type = venue_info['venue_type']
+	self.login = venue_info['login']
+	self.password = venue_info['password']
+	self.monday = venue_info['monday']
+	self.tuesday = venue_info['tuesday']
+	self.wednesday = venue_info['wednesday']
+	self.thursday = venue_info['thursday']
+	self.friday = venue_info['friday']
+	self.saturday = venue_info['saturday']
+	self.sunday = venue_info['sunday']
+	self.food_drink = venue_info['food_drink']
+        self.active = active
+    
+    def is_active(self):
+        return self.active
 
 if __name__ == "__main__":
     app.run(port=61004)
