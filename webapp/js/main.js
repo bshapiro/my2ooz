05d1237e1043cd687580913a0a9ce085f7024e23
Benjamin Shapiro <benj@berkeley.edu>
2013-05-07 00:13:37 -0700

    added js and some extra css

---
+++ webapp/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready( function () {
+
+
+    $("#login_items").mouseover(function() {
+        $("#login_box").fadeIn();
+    });
+
+     $("#login_items").mouseleave(function() {
+        $("#login_box").fadeOut();
+    });
+
+     $("#register_items").mouseover(function() {
+        $("#register_box").fadeIn();
+    });
+
+     $("#register_items").mouseleave(function() {
+        $("#register_box").fadeOut();
+    });
+
+});
