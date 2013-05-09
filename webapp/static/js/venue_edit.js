c0661259ffe3eb80dc0f90f0eb70e67212eaee9e
Benjamin Shapiro <benj@berkeley.edu>
2013-05-08 23:08:36 -0700

    save cleanup

--- webapp/static/js/venue_edit.js
+++ webapp/static/js/venue_edit.js
@@ -21,61 +21,57 @@ $(document).ready(function () {
         $("#logout_box").fadeOut();
     });
 
-
-      $('#login_header').click(function() {
+    $('#login_header').click(function() {
         if ($('#login_information').is(':visible')) {
             $('#login_information').hide('slide');
         } else {
             $('#login_information').show('slide');
         }
-     });
+    });
 
 
-     $('#venue_header').click(function() {
+    $('#venue_header').click(function() {
         if ($('#venue_information').is(':visible')) {
             $('#venue_information').hide('slide');
         } else {
             $('#venue_information').show('slide');
         }
-     });
+    });
 
-     $('#calendar_header').click(function() {
+    $('#calendar_header').click(function() {
         if ($('#calendar_information').is(':visible')) {
             $('#calendar_information').hide('slide');
         } else {
             $('#calendar_information').show('slide');
         }
-     });
-
-
-
+    });
 
     var buttonpressed;
     $('.save').click( function() {
-	buttonpressed = $(this).attr('id');	
+        buttonpressed = $(this).attr('id');
     });
 
     $("#form_object").submit(function(e){
-           $.post(
-               "/my2ooz/venue_update",
-               $("#form_object").serialize(),
-                function(data){
-                   console.log(data);
-               }
-           );
-	   console.log(buttonpressed); 
+        $.post(
+           "/my2ooz/venue_update",
+           $("#form_object").serialize(),
+            function(data){
+               console.log(data);
+           }
+       );
 
-	   if (buttonpressed == "save1") {
-           $('#success1').show('slow');
-	   setTimeout(function(){
-		$('#success').hide('slow');
-	   }, 5000);
-	   } else {
-	   $('#success2').show('slow');
+        if (buttonpressed == "save1") {
+          $('#success1').show('slow');
+          setTimeout(function(){
+            $('#success1').hide('slow');
+          }, 5000);
+        } else {
+          $('#success2').show('slow');
            setTimeout(function(){
-                $('#success').hide('slow');
+                $('#success2').hide('slow');
            }, 5000);
-	   }
-           return false;
+        }
+
+        return false;
     });
 });
