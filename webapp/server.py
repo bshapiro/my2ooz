1496afd17e191e85971c890e54581a0b40af68ae
Benjamin Shapiro <benj@berkeley.edu>
2013-05-08 21:45:25 -0700

    removed refresh from updating venue

--- webapp/server.py
+++ webapp/server.py
@@ -98,8 +98,8 @@ def insert_venue(connection, parameters):
         data = connection.execute(query)
         return data
     except Exception, ex:
-	print query
-	print ex
+        print query
+        print ex
         return "hi"
 
 
@@ -152,70 +152,17 @@ def load_user(userid):
 @login_required
 def logout():
     if logout_user():
-        return "User successfully logged out."
+        return render_template("main.html")
     else:
-        return "There was an error logging out the user."
+        return render_template("main.html")
 
 
 class User:
 
     def __init__(self, venue_info, active=True):
         if venue_info is not None:
-            self.venue_id = venue_info['venue_id']
-	    self.venue_name = venue_info['venue_name']
-            self.address_line_1 = venue_info['address_line_1']
-            self.address_line_2 = venue_info['address_line_2']
-            self.city = venue_info['city']
-            self.state = venue_info['state']
-            self.zipcode = venue_info['zipcode']
-            self.venue_phone = venue_info['venue_phone']
-            self.type = venue_info['type']
-
-            self.mon_open_hour = venue_info['mon_open_hour']
-            self.mon_open_am_pm = venue_info['mon_open_am_pm']
-            self.mon_close_hour = venue_info['mon_close_hour']
-            self.mon_close_am_pm = venue_info['mon_close_am_pm']
-
-            self.tues_open_hour = venue_info['tues_open_hour']
-            self.tues_open_am_pm = venue_info['tues_open_am_pm']
-            self.tues_close_hour = venue_info['tues_close_hour']
-            self.tues_close_am_pm = venue_info['tues_close_am_pm']
-
-            self.wed_open_hour = venue_info['wed_open_hour']
-            self.wed_open_am_pm = venue_info['wed_open_am_pm']
-            self.wed_close_hour = venue_info['wed_close_hour']
-            self.wed_close_am_pm = venue_info['wed_close_am_pm']
-
-            self.thurs_open_hour = venue_info['thurs_open_hour']
-            self.thurs_open_am_pm = venue_info['thurs_open_am_pm']
-            self.thurs_close_hour = venue_info['thurs_close_hour']
-            self.thurs_close_am_pm = venue_info['thurs_close_am_pm']
-
-            self.fri_open_hour = venue_info['fri_open_hour']
-            self.fri_open_am_pm = venue_info['fri_open_am_pm']
-            self.fri_close_hour = venue_info['fri_close_hour']
-            self.fri_close_am_pm = venue_info['fri_close_am_pm']
-
-            self.sat_open_hour = venue_info['sat_open_hour']
-            self.sat_open_am_pm = venue_info['sat_open_am_pm']
-            self.sat_close_hour = venue_info['sat_close_hour']
-            self.sat_close_am_pm = venue_info['sat_close_am_pm']
-
-            self.sun_open_hour = venue_info['sun_open_hour']
-            self.sun_open_am_pm = venue_info['sun_open_am_pm']
-            self.sun_close_hour = venue_info['sun_close_hour']
-            self.sun_close_am_pm = venue_info['sun_close_am_pm']
-
-            self.website = venue_info['website']
-            self.yelp = venue_info['yelp']
-            self.photos = venue_info['photos']
-            self.other_info = venue_info['other_info']
-            self.discounts = venue_info['discounts']
-            self.contact = venue_info['contact']
-            self.contact_phone = venue_info['contact_phone']
-            self.email = venue_info['email']
-            self.password = venue_info['password']
             self.venue_info = venue_info
+            self.venue_id = venue_info['venue_id']
             self.authenticated = True
         else:
             self.authenticated = False
--- webapp/static/js/venue_edit.js
+++ webapp/static/js/venue_edit.js
@@ -5,11 +5,23 @@ $(document).ready(function () {
       type: "POST",
       url: "/my2ooz/venue_info",
     }).done(function( data) {
-      elements = $('[name]');  
+      elements = $('[name]');
       console.log(elements);
       for (var i = 0; i < elements.length; i++) {
         $(elements[i]).attr('value', data[$(elements[i]).attr('name')]);
       }
     });
 
+
+    $("#form_object").submit(function(){
+           $.post(
+               "/my2ooz/venue_update",
+               $("#form_object").serialize(),
+                function(data){
+                   console.log(data);
+               }
+           );
+           $('.success').show();
+           return false;
+    });
 });
