8409344e10299fe605c780bedec9fa752f003c7a
Benjamin Shapiro <benj@berkeley.edu>
2013-05-08 22:37:57 -0700

    minor style fixes

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -9,6 +9,7 @@ $(document).ready( function () {
         $("#login_box").fadeOut();
     });
 
+
      $("#register_items").mouseover(function() {
         $("#register_box").fadeIn();
     });
@@ -16,31 +17,5 @@ $(document).ready( function () {
      $("#register_items").mouseleave(function() {
         $("#register_box").fadeOut();
     });
-
-     $('#login_header').click(function() {
-        if ($('#login_information').is(':visible')) {
-            $('#login_information').hide('slide');
-        } else {
-            $('#login_information').show('slide');
-        }
-     });
-
-
-     $('#venue_header').click(function() {
-        if ($('#venue_information').is(':visible')) {
-            $('#venue_information').hide('slide');
-        } else {
-            $('#venue_information').show('slide');
-        }
-     });
-
-     $('#calendar_header').click(function() {
-        if ($('#calendar_information').is(':visible')) {
-            $('#calendar_information').hide('slide');
-        } else {
-            $('#calendar_information').show('slide');
-        }
-     });
-
 
 });
--- webapp/static/js/venue_edit.js
+++ webapp/static/js/venue_edit.js
@@ -13,6 +13,42 @@ $(document).ready(function () {
     });
 
 
+    $("#logout_items").mouseover(function() {
+        $("#login_box").fadeIn();
+    });
+
+     $("#logout_items").mouseleave(function() {
+        $("#login_box").fadeOut();
+    });
+
+      $('#login_header').click(function() {
+        if ($('#login_information').is(':visible')) {
+            $('#login_information').hide('slide');
+        } else {
+            $('#login_information').show('slide');
+        }
+     });
+
+
+     $('#venue_header').click(function() {
+        if ($('#venue_information').is(':visible')) {
+            $('#venue_information').hide('slide');
+        } else {
+            $('#venue_information').show('slide');
+        }
+     });
+
+     $('#calendar_header').click(function() {
+        if ($('#calendar_information').is(':visible')) {
+            $('#calendar_information').hide('slide');
+        } else {
+            $('#calendar_information').show('slide');
+        }
+     });
+
+
+
+
     $("#form_object").submit(function(){
            $.post(
                "/my2ooz/venue_update",
